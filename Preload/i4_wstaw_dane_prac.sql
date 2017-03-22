749c1385752e04680a3e642bd7c70c9c072a9788
RobekX <robek005@gmail.com>
2017-03-22 09:35:24 +0100

    biblioteka do zad4

---
+++ Preload/i4_wstaw_dane_prac.sql
@@ -0,0 +1,35 @@
+---- 1
+--PRINT 'Czeœæ, to ja';
+
+-- 2
+DECLARE @ZMIENNA INT;
+SET @ZMIENNA = 1;
+
+---- 3
+--IF (@ZMIENNA = 1)
+--	PRINT 'ZMIENNA ROWNA ZERO!'
+--ELSE
+--	PRINT 'ZMIENNA ROZNA OD ZERA!'
+
+---- 4
+--WHILE @ZMIENNA < 5
+--BEGIN
+--   PRINT 'zmienna ma wartosc' + STR(@ZMIENNA);
+--   SET @ZMIENNA = @ZMIENNA + 1;
+--END;
+
+-- 5
+--SET @ZMIENNA = 3;
+--WHILE @ZMIENNA < 8
+--BEGIN
+--   PRINT @ZMIENNA;
+--   IF (@ZMIENNA = 3)
+--		PRINT 'POCZATEK';
+--	IF (@ZMIENNA = 5)
+--		PRINT 'SRODEK';
+--	IF (@ZMIENNA = 7)
+--		PRINT 'KONIEC'
+--   SET @ZMIENNA = @ZMIENNA + 1;
+--END;
+
+-- 6
